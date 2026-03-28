7ffcfb1cff7e65fe7a391b7f631852770e3983bf
Jocelyn <duong.huan2122000@gmail.com>
2026-03-28 10:31:44 +0700

    feat: maximize main window on show

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,8 +6,6 @@ import icon from '../../resources/icon.png?asset';
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
@@ -18,6 +16,7 @@ function createWindow(): void {
   });
 
   mainWindow.on('ready-to-show', () => {
+    mainWindow.maximize();
     mainWindow.show();
   });
 
